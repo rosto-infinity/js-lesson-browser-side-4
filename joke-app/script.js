eb784bb41028c6d24c9f22890e854de2339a260d
rosto-infinity <rosto.infocus@gmail.com>
2023-05-18 18:35:58 -1100

    joke

---
+++ joke-app/script.js
@@ -0,0 +1,27 @@
+// https://api.blablagues.net/?rub=blagues
+
+const header = document.getElementById("header");
+const content = document.getElementById("content");
+
+
+function getJoke(){
+
+    fetch("https://api.blablagues.net/?rub=blagues")
+    .then((res)=> res.json())
+    .then((data) => {
+        console.log(data.data.content);
+        
+        header.textContent = data.data.content.text_head;
+        content.textContent = data.data.content.text !=="" 
+        ?data.data.content.text
+        :data.data.content.text_hidden ;
+    });
+}
+getJoke();
+
+btn.addEventListener('click', getJoke
+//  ()=> console.log('test')
+ 
+ 
+ );
+
