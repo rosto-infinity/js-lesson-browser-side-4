0c4eca2662b8e154355d1ec61e52c10f25982488
rostodev <rostodev@gmail.com>
2023-05-23 21:28:56 -1100

    joke new

--- joke-app/script.js
+++ joke-app/script.js
@@ -3,25 +3,73 @@
 const header = document.getElementById("header");
 const content = document.getElementById("content");
 
+function Blague(){
+fetch("https://api.blablagues.net/?rub=blagues")
+.then((res) => res.json())
+.then((info)=>{
+     console.log(info.data);
+     const blague=info.data.content
+     header.textContent = blague.text_head;
+     content.textContent = blague !==""
+     ?blague.text_hidden 
+     : blague.text
+     ;
 
-function getJoke(){
-
-    fetch("https://api.blablagues.net/?rub=blagues")
-    .then((res)=> res.json())
-    .then((data) => {
-        console.log(data.data.content);
-        
-        header.textContent = data.data.content.text_head;
-        content.textContent = data.data.content.text !=="" 
-        ?data.data.content.text
-        :data.data.content.text_hidden ;
-    });
+     
+    }
+    )
 }
-getJoke();
+Blague();
+    btn.addEventListener('click', Blague);
+    
 
-btn.addEventListener('click', getJoke
-//  ()=> console.log('test')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const header = document.getElementById("header");
+// const content = document.getElementById("content");
+
+
+// function getJoke(){
+
+//     fetch("https://api.blablagues.net/?rub=blagues")
+//     .then((res)=> res.json())
+//     .then((data) => {
+//         console.log(data.data.content);
+        
+//         header.textContent = data.data.content.text_head;
+//         content.textContent = data.data.content.text !=="" 
+//         ?data.data.content.text
+//         :data.data.content.text_hidden ;
+//     });
+// }
+// getJoke();
+
+// btn.addEventListener('click', getJoke
+// //  ()=> console.log('test')
  
  
- );
+//  );
 
